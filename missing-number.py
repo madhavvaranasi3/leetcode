ccc2a86ee8b3a0b3fac07c64f79232629a3cf082
Madhav Varanasi <62279010+madhavvaranasi3@users.noreply.github.com>
2024-03-18 13:53:04 +0530

    Add files via upload

---
+++ missing-number.py
@@ -0,0 +1,9 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n=len(nums)
+        sum1 = int((n*(n+1))/2)
+        sum2 = 0
+        for i in nums:
+            sum2 += i
+        return (sum1 - sum2)
+        
