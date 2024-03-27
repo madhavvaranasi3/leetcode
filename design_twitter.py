0f84432f450c1698a8d6fe3d5f261db6a3166f9e
Madhav Varanasi <62279010+madhavvaranasi3@users.noreply.github.com>
2024-03-27 15:12:31 +0530

    Add files via upload

---
+++ design_twitter.py
@@ -0,0 +1,47 @@
+class Twitter:
+    tweetList = []
+    followers = {}
+    def __init__(self):
+        self.tweetList = []
+        self.followers = {}
+
+    def postTweet(self, userId: int, tweetId: int) -> None:
+        self.tweetList.append([userId, tweetId])
+        
+    def getNewsFeed(self, userId: int) -> List[int]:
+        lst = []
+        for i in range(len(self.tweetList) - 1, -1, -1):
+            if self.tweetList[i][0] == userId:
+                lst.append(self.tweetList[i][1])
+                if(len(lst) == 10):
+                    break
+            else:
+                if userId in self.followers:
+                    val = self.followers[userId]
+                    if self.tweetList[i][0] in val:
+                        lst.append(self.tweetList[i][1])
+                        if(len(lst) == 10):
+                            break
+        return lst
+    def follow(self, followerId: int, followeeId: int) -> None:
+        if followerId in self.followers:
+            val = self.followers[followerId]
+            val.append(followeeId)
+            self.followers[followerId] = val
+        else:
+            lst = [followeeId]
+            self.followers[followerId] = lst
+
+    def unfollow(self, followerId: int, followeeId: int) -> None:
+        if followerId in self.followers:
+            val = self.followers[followerId]
+            val.remove(followeeId)
+            self.followers[followerId] = val
+
+
+# Your Twitter object will be instantiated and called as such:
+# obj = Twitter()
+# obj.postTweet(userId,tweetId)
+# param_2 = obj.getNewsFeed(userId)
+# obj.follow(followerId,followeeId)
+# obj.unfollow(followerId,followeeId)
